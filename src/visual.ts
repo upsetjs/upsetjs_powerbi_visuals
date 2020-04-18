018af715473fcc0a5b4de7d6e1166b2ecb4645ff
Samuel Gratzl <sam@sgratzl.com>
2020-04-18 17:03:22 +0200

    sync selection

--- src/visual.ts
+++ src/visual.ts
@@ -9,66 +9,164 @@ import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 
 import VisualSettings, { fixOrder } from './VisualSettings';
-import { renderUpSet, asSets, ISet, UpSetProps } from '@upsetjs/bundle';
+import {
+  renderUpSet,
+  asSets,
+  ISet,
+  UpSetProps,
+  ISetLike,
+  generateCombinations,
+  ISetCombinations,
+} from '@upsetjs/bundle';
 
-interface IPowerBISet extends ISet<powerbi.PrimitiveValue> {
+declare type IPowerBIElem = powerbi.visuals.ISelectionId | powerbi.PrimitiveValue;
+declare type IPowerBIElems = ReadonlyArray<IPowerBIElem>;
+
+interface IPowerBISet extends ISet<IPowerBIElem> {
   value: powerbi.DataViewValueColumn;
+}
+
+function isSelection(s: powerbi.extensibility.ISelectionId): s is powerbi.visuals.ISelectionId {
+  return s != null && typeof (<powerbi.visuals.ISelectionId>s).includes === 'function';
 }
 
 export class Visual implements IVisual {
   private readonly target: HTMLElement;
+  private readonly interactive: boolean;
   private settings: VisualSettings = <VisualSettings>VisualSettings.getDefault();
+  private readonly selectionManager: powerbi.extensibility.ISelectionManager;
+  private readonly host: powerbi.extensibility.visual.IVisualHost;
+
+  private props: UpSetProps<IPowerBIElem> = { sets: [], width: 100, height: 100 };
 
   constructor(options: VisualConstructorOptions) {
     this.target = options.element;
+    this.interactive = options.host.allowInteractions;
+    this.selectionManager = options.host.createSelectionManager();
+    this.host = options.host;
+  }
+
+  private setSelection = (selection: ISetLike<IPowerBIElem> | null) => {
+    if (!selection) {
+      this.selectionManager.clear().then(() => {
+        this.props.selection = null;
+        this.render();
+      });
+    } else {
+      this.selectionManager.select(<powerbi.visuals.ISelectionId[]>selection.elems).then(() => {
+        this.props.selection = selection;
+        this.render();
+      });
+    }
+  };
+
+  private render() {
+    renderUpSet(this.target, this.props);
   }
 
   update(options: VisualUpdateOptions) {
     const dataView = options.dataViews[0];
     this.settings = Visual.parseSettings(dataView);
 
-    const { sets, elems } = this.extractSets(dataView.categorical!);
+    const areDummyValues = dataView.categorical!.categories.length === 0;
 
-    const selection = this.deriveSelection(elems, dataView.categorical!);
+    const elems = this.extractElems(dataView.categorical!);
+    const sets = this.extractSets(elems, dataView.categorical!);
+    const combinations = generateCombinations(
+      sets,
+      Object.assign({}, this.settings.combinations, {
+        order: fixOrder(this.settings.combinations.order),
+        elems,
+      })
+    );
 
-    const props: UpSetProps<powerbi.PrimitiveValue> = Object.assign(
+    let selection: IPowerBIElems = this.deriveSelection(elems, dataView.categorical!);
+    if (!selection && !areDummyValues && this.interactive) {
+      selection = this.fromSelection(elems);
+    }
+
+    this.props = Object.assign(
       {
         sets,
         width: options.viewport.width,
         height: options.viewport.height,
-        combinations: Object.assign({}, this.settings.combinations, {
-          order: fixOrder(this.settings.combinations.order),
-          elems,
-        }),
-        selection,
+        combinations,
+        selection: this.findSet(selection, sets, combinations),
         exportButtons: false,
       },
       this.settings.theme.dropDefaults(),
       this.settings.style
     );
 
-    renderUpSet(this.target, props);
+    if (!areDummyValues && this.interactive) {
+      this.props.onClick = this.setSelection;
+    }
+
+    console.log('render');
+    this.render();
   }
 
-  private deriveSelection(elems: ReadonlyArray<powerbi.PrimitiveValue>, data: powerbi.DataViewCategorical) {
+  private deriveSelection(elems: IPowerBIElems, data: powerbi.DataViewCategorical) {
     if (data.values.length === 0 || data.values[0].highlights == null) {
       return undefined;
     }
     return data.values[0].highlights.map((v, i) => (v === null ? null : elems[i])).filter((v) => v !== null);
   }
 
-  private extractSets(
-    data: powerbi.DataViewCategorical
-  ): { sets: ReadonlyArray<IPowerBISet>; elems: ReadonlyArray<powerbi.PrimitiveValue> } {
-    const defaultElems = () => {
-      if (data.values.length === 0) {
-        return [];
-      }
-      return data.values[0].values.map((_, i) => i);
-    };
-    const elems: powerbi.PrimitiveValue[] = data.categories.length > 0 ? data.categories[0].values : defaultElems();
+  private fromSelection(elems: IPowerBIElems): IPowerBIElems | undefined {
+    const sel = this.selectionManager.getSelectionIds();
 
-    const sets = asSets(
+    if (sel.length === 0) {
+      return undefined;
+    }
+    return elems.filter((elem) =>
+      sel.some((s) => elem === s || (isSelection(s) && s.includes(<powerbi.visuals.ISelectionId>elem)))
+    );
+  }
+
+  private findSet(
+    selection: IPowerBIElems | undefined,
+    sets: ReadonlyArray<IPowerBISet>,
+    combinations: ISetCombinations<IPowerBIElem>
+  ) {
+    if (!selection || selection.length === 0) {
+      return undefined;
+    }
+    const toFind = new Set(selection);
+    const set = sets.find((s) => {
+      if (s.cardinality !== selection.length) {
+        return false;
+      }
+      return s.elems.every((e) => toFind.has(e));
+    });
+    if (set) {
+      return set;
+    }
+    const c = combinations.find((s) => {
+      if (s.cardinality !== selection.length) {
+        return false;
+      }
+      return s.elems.every((e) => toFind.has(e));
+    });
+    if (c) {
+      return c;
+    }
+    return selection;
+  }
+
+  private extractElems(data: powerbi.DataViewCategorical): IPowerBIElems {
+    if (data.categories.length === 0) {
+      return data.values.map((_, i) => i);
+    }
+    const cat = data.categories[0]!;
+    if (!this.interactive) {
+      return cat.values;
+    }
+    return cat.values.map((_, i) => this.host.createSelectionIdBuilder().withCategory(cat, i).createSelectionId());
+  }
+
+  private extractSets(elems: IPowerBIElems, data: powerbi.DataViewCategorical): ReadonlyArray<IPowerBISet> {
+    return asSets(
       data.values
         .map((value) => {
           const vs = value.values;
@@ -80,7 +178,6 @@ export class Visual implements IVisual {
         })
         .reverse()
     );
-    return { sets, elems };
   }
 
   private static parseSettings(dataView: DataView): VisualSettings {
