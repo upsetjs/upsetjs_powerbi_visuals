9dd43277c59757104daba6e552505fc17907e21d
Samuel Gratzl <sam@sgratzl.com>
2020-05-03 16:03:58 +0200

    fix formatting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
-{
-  "printWidth": 120,
-  "semi": true,
-  "singleQuote": true,
-  "trailingComma": "es5"
-}
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
